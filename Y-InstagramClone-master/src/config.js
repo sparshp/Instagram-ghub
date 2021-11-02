72541a0fdb1c8e05e07b63201428828d4394ebbf
Sparsh Prajapati <71871246+sparshp@users.noreply.github.com>
2021-11-02 13:24:35 +0530

    Update config.js

--- Y-InstagramClone-master/src/config.js
+++ Y-InstagramClone-master/src/config.js
@@ -1,12 +1,6 @@
 import firebase from "firebase";
 var firebaseConfig = {
-  apiKey: "AIzaSyCAPQ_aUjWs9r0A48tAOngiJWwoUTILn3g",
-  authDomain: "instagram-ghub.firebaseapp.com",
-  projectId: "instagram-ghub",
-  storageBucket: "instagram-ghub.appspot.com",
-  messagingSenderId: "447113115455",
-  appId: "1:447113115455:web:8d0864ab48c5051250c58a",
-  measurementId: "G-VJK3T7TT74"
+  //your firebase config file
 };
 
 firebase.initializeApp(firebaseConfig);
